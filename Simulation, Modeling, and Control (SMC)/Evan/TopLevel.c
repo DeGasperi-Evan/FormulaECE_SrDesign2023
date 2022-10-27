9486868fb0a9ba2451f3533f60bca2fbed45a98d
DeGasperi-Evan <ejd7190@utulsa.edu>
2022-10-26 22:48:10 -0500

    Modified TopLevel.c to communicate with hardware and Pozyx.

--- Simulation, Modeling, and Control (SMC)/Evan/TopLevel.c
+++ Simulation, Modeling, and Control (SMC)/Evan/TopLevel.c
@@ -1,18 +1,43 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#include <time.h>
-//#include "Initialize.h"
+#include <string.h>
+#include "comms.h"
+
+#define BUFFSIZE 512
+
+/* Hardware interfacing and communication stuff*/
+#define INFILENAME "pytoc"
+#define OUTFILENAME "ctopy"
+
+struct Pipes openPipes() {
+    struct Pipes ret = {0,0};
+    if ((ret.in = open(INFILENAME, O_RDONLY)) < 0) {
+        fprintf(stderr,"Error: open infile");
+    	return ret;
+    }
+
+    if ((ret.out = open(OUTFILENAME, O_WRONLY)) < 0) {
+        fprintf(stderr,"Error: open outfile");
+    	return ret;
+	}
+    return ret;
+}
+
+int wait_loop0 = 10000/10;
+int wait_loop1 = 6000;
 
 double driveTable[4][2] = {
     {78, 0.92}, {80, 1.56}, {82, 2.19}, {84, 2.61}
 };
 double steerTable[15][2] = {
-    {60, 45}, {62, 45}, {64, 55}, {66, 60}, {68, 70},
-    {70, 75}, {72, 80}, {74, 90}, {76, 95}, {78, 105},
-    {80, 110}, {82, 115}, {84, 120}, {86, 125}, {88, 130}
+  {54, 45}, {56, 45}, {58, 55},  {60, 60},
+  {62, 70}, {64, 75}, {66, 80}, {68, 85},
+  {70, 90}, {72, 95},  {74, 105},  {76, 110}, 
+  {80, 115}, {82, 120}, {84, 125}, {86, 130}
 };
-double controlArray[14][9] = {
+
+double controlArray[14][9] = {   
     {1,    1,    8.5,  1,    3,    0,    -1,   1,    3   }, 
     {0,    3,    3,    2,    1,    1,    0,    3,    1   },
     {1,    3,    1,    9.5,  1.5,  1,    0,    9.5,  1.5 },
@@ -27,30 +52,35 @@ double controlArray[14][9] = {
     {1,    9,    11.5, 6,    10.5, -1,   0,    6,    10.5},
     {1,    6,    10.5, 3,    10.5, -1,   0,    3,    10.5},
     {0,    3,    8.5,  2,    1,    0,    -1,   1,    8.5 }
-/*  {1, 1, 9, 1, 2, 0, -1, 1, 2},
-    {0, 2.1, 2.1, 1, 1, 1, 0, 2, 1},
-    {1, 2, 1, 6, 2, 1, 0, 6, 2},
-    {1, 6, 2, 10, 1.5, 1, 0, 9.8, 1.5},
-    {0, 9.9, 2.6, 1, 1, 0, 1, 10.8, 2.5},
-    {1, 11, 2.5, 11, 5, 0, 1, 11, 5},
-    {0, 10, 4.9, 1, 1, 0, -1, 9, 5},
-    {0, 7.9, 5.1, 1, 0, -1, 0, 8, 4},
-    {1, 8, 4, 4, 4.5, -1, 0, 4.2, 4.5},
-    {0, 4.1, 5.6, 1, 0, 0, 1, 3, 5.3},
-    {1, 3, 5.5, 3, 8, 0, 1, 3, 7.6},
-    {0, 4.1, 7.9, 1, 0, 1, 0, 3.8, 9},
-    {1, 4, 9, 10, 9, 1, 0, 10.1, 9},
-    {0, 10, 10.5, 1.5, 1, -1, 0, 10, 12},
-    {1, 10, 12, 6, 10, -1, 0, 6, 10},
-    {1, 6, 10, 2, 10, -1, 0, 2, 10},
-    {0, 2, 9, 1, 1, 0, -1, 1, 9}*/
 };
+
+/* Line Test
+double controlArray[14][9] = {   
+    {1,    1,    8.5,  1,    3,    0,    -1,   1,    3   }
+};
+*/
+
+/* Circle Test
+double controlArray[14][9] = {
+    {0, 2.5, 8.5, 1.5, 1, 0, -1, 1, 100}
+};
+*/
+
+/* Mini Track Test
+double controlArray[14][9] = {
+    {1,    1,    8.5,  1,    6,    0,    -1,   1,    6   },
+    {0,    2.5   6,    1.5,  1,    0,     1,   4,    6   },
+    {1,    4,    6,    4,    8.5,  0,     1,   4,    8.5 },
+    {0,    2.5,  8.5,  1.5,  1,    0,    -1,   1,    8.5 }
+};
+*/
+
 int controlIndex = 0;
 double kp = 0;
 double kd = 0;
 double ki = 0;
 int lap = 0;
-double dt = 0.1;
+double dt = 0.05;
 double v = 1.0;
 double carLocation[2] = {1, 6};
 double thetaC = 3 * M_PI/2;
@@ -65,8 +95,10 @@ double resetIntegral = 0;
 double PWM[2] = {0, 0};
 double X_old_predicted[2] = {0, 0};
 int counter = 0;
+float encoderData;
 
 void initialize();
+void AutoCodeEncoder(double U[], float encoderData);
 void estimatedPos(double U[], double X[]);
 void updatePosPozyx(double X[], double pozyxInput[]);
 void control(double X[]);
@@ -85,6 +117,7 @@ void insert(); // Function used to insert the element into the queue
 void delet(); // Function used to delete the elememt from the queue
 void serch(int index, double out[2]); // Function used to display all the elements in the queue according to FIFO rule
 int size();
+void wait(int seconds);
 
 double out[2] = {0, 0};
 
@@ -146,21 +179,46 @@ int size() {
 /* End Queue */
 
 void main() {
+    /*Stuff to send/receive data for HW interfacing*/
+    struct Pipes pip;
+	pip = openPipes();
+    double heading;
+	double posx;
+	double posy;
+
+	char buf[BUFFSIZE] = {0};
+	char outbuf[BUFFSIZE] = {0};
+    /* END of HW interfacing*/
+
     initialize();
     while(lap < 4 ) {
+        readInPipe(pip, buf, BUFFSIZE);
+        if(buf[0] != 0x0)
+        {
+            sscanf(strtok(buf, ","), "%lf", &posx);
+            printf("%.4lf\n", posx);
+            sscanf(strtok(NULL, ","), "%lf", &posy);
+            printf("%.4lf\n", posy);
+            sscanf(strtok(NULL, ","), "%lf", &heading);
+            printf("%.4lf\n", heading);
+        }
+        double pozyxInfo[2] = {posx, posy}; 
+
+        AutoCodeEncoder(U, encoderData);
         estimatedPos(X, U);
         updatePosPozyx(X, pozyxInput);
         control(X);
         sendMotorPWM(U);
+        sprintf(outbuf, "%lf,%lf",PWM[0],PWM[1]);
+        writeOutPipe(pip, outbuf, BUFFSIZE);
         if(counter > 2/dt) {
             delet();
         }
         double queueInsert[3] = {counter, X[0],X[1]};
         insert(queueInsert);
         counter +=1; 
-
         printf("Coordinates: [%lf, %lf] Drive PWM: %lf Steer PWM: %lf \n", X[0], X[1], PWM[0], PWM[1]);
-        //_sleep(100);
+        //wait(1);
     }
 }
 
@@ -175,6 +233,39 @@ void initialize() {
     pozyxInput[2] = 0;
 }
 
+void AutoCodeEncoder(double U[2], float encoderData)
+{
+    static int counter1;
+    static int counter2;
+    static float buffer;
+    encoderData = -1; // for testing
+    while(counter1 < 3 && counter2 < 5)
+    {
+        if (0 < encoderData && encoderData < 500)
+        {
+            buffer += 46.019423 / encoderData;
+            counter1 += 1;
+        }
+        counter2 += 1;
+    }
+
+    if (counter1 == 3)
+    {
+        if (0 < buffer && buffer < 10)
+        {
+            U[0] = buffer/3;
+        }
+        counter1 = 0;
+        counter2 = 0;
+    }
+    else {
+        U[0] = U[0];
+        counter1 = 0;
+        counter2 = 0;  
+    }
+  
+}
+
 void estimatedPos(double X[], double U[]) {
     v = U[0];
     thetaS = U[1];
@@ -183,10 +274,10 @@ void estimatedPos(double X[], double U[]) {
     X[0] = X_dot[0] * dt + X[0];
     X[1] = X_dot[1] * dt + X[1];
     X[2] = X_dot[2] * dt + X[2];
-    if(X[2] > 2*M_PI) {
+    while(X[2] > 2*M_PI) {
         X[2] -= 2*M_PI;
     }
-    if(X[2] < 0) {
+    while(X[2] < 0.0) {
         X[2] += 2*M_PI;
     }
 }
@@ -241,8 +332,9 @@ void control(double X[]) {
         double error = Rcar - Rcirc;
         kp = lambda * 0.4;
         kd = lambda * 0.5;
-        ki = lambda * 0.0003;
+        ki = lambda * 0.01;
         if(resetIntegral == 1) {
+            integral = 0;
             derivCirc = 0;
         }
         else {
@@ -264,7 +356,7 @@ void control(double X[]) {
         double W2[2] = {controlArray[controlIndex][3], controlArray[controlIndex][4]};
         double xdiff = W2[0] - W1[0];
         double ydiff = W2[1] - W1[1];
-        double angle = atan(ydiff/xdiff);
+        double angle = atan2(ydiff,xdiff);
         double R[3][3] = {
             {cos(angle), -sin(angle), W1[0]},
             {sin(angle), cos(angle), W1[1]},
@@ -279,8 +371,8 @@ void control(double X[]) {
             }
         }
         D = inverse[1][0] * Xmatrix[0] + inverse[1][1] * Xmatrix[1] + inverse[1][2] * Xmatrix[2];
-        kp = -0.5;
-        kd = -0.4;
+        kp = -0.4;
+        kd = -0.5;
         derivLine = (D-oldDLine)/dt;
         thetaS = D * kp + kd * derivLine;
         U[0] = v;
@@ -303,35 +395,71 @@ void sendMotorPWM(double U[]) {
         }
     }
     steerAngle += 90.0;
-    for(int i = 0; i < sizeof(driveTable) / sizeof(driveTable[0]); i++) {
-        if(driveSpeedPWM == 0 && U[0] - 0.3 < driveTable[i][1] && U[0] + 0.3 > driveTable[i][1]) {
-            driveSpeedPWM = driveTable[i][0];
-            //U[0] = driveTable[i][1];
+// Scan steer curve
+  for(int i = 0; i < sizeof(steerTable) / sizeof(steerTable[0]); i++)
+  {
+    if(abs(steerAngle - steerTable[i][1] ) < 7 )//steerAngle - 7 < steerTable[i][1] && steerAngle + 7 > steerTable[i][1])
+    {
+      steerAnglePWM = steerTable[i][0];
+      U[1] = (steerTable[i][1] - 90) / 57.295779513082323;
+      break;
+    }
+  }
+
+  if (steerAngle > 130.0) {
+    steerAnglePWM = 88;
+    U[1] = (steerTable[14][1] - 90) / 57.295779513082323;
+  } else if (steerAngle < 45.0) {
+    steerAnglePWM = 60;
+    U[1] = (steerTable[0][1] - 90) / 57.295779513082323;
+  }
+
+  if (driveSpeedPWM == 0)
+  {
+    driveSpeedPWM = 78;
+    U[0] = driveTable[0][1];
+  }
+
+  if (steerAnglePWM == 0)
+  {
+    steerAnglePWM = 74;
+    U[1] = (steerTable[7][1] - 90) / 57.295779513082323;
+  }
+
+  PWM[0] = driveSpeedPWM;
+  PWM[1] = steerAnglePWM;
+}
+
+
+/*Some communication/HW interfacing stuff*/
+int readInPipe(struct Pipes file, char *buf, int bufsize) {
+    return read(file.in, buf, bufsize);
+}
+
+void writeOutPipe(struct Pipes file, char *outbuf, int bufsize) {
+	write(file.out, outbuf, bufsize);
+}
+
+void closePipes(struct Pipes file) {
+    unlink(INFILENAME);
+    unlink(OUTFILENAME);
+    close(file.in);
+    close(file.out);
+}
+
+/* Time Delay method*/
+void wait( int seconds )
+{   // this function needs to be finetuned for the specific microprocessor
+    int i, j, k;
+    for(i = 0; i < seconds; i++)
+    {
+        for(j = 0; j < wait_loop0; j++)
+        {
+            for(k = 0; k < wait_loop1; k++)
+            {   // waste function, volatile makes sure it is not being optimized out by compiler
+                int volatile t = 120 * j * i + k;
+                t = t + 5;
+            }
         }
     }
-    for(int i = 0; i < sizeof(steerTable) / sizeof(steerTable[0]); i++) {
-        if(steerAnglePWM == 0 && steerAngle - 7 < steerTable[i][1] && steerAngle + 7 > steerTable[i][1]) {
-            steerAnglePWM = steerTable[i][0];
-            //U[1] = driveTable[i][1];
-        }
-    }
-    if (steerAngle > 130.0) {
-        steerAnglePWM = 88;
-        //U[1] = steerTable[14][1];
-    } else if (steerAngle < 45.0) {
-        steerAnglePWM = 60;
-        //U[1] = steerTable[0][1];
-    }
-
-    if(driveSpeedPWM == 0) {
-        driveSpeedPWM == 78;
-        //[0] = driveTable[0][1];
-    }
-
-    if(steerAnglePWM == 0) {
-        steerAnglePWM = 74;
-        //U[1] = steerTable[7][1];
-    }
-    PWM[0] = driveSpeedPWM;
-    PWM[1] = steerAnglePWM;
 }
